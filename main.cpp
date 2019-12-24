153a6779b20bd6d251ffcbbd45ad4fa854e966bc
MatthewRose <matthewaerose@gmail.com>
2019-12-24 08:33:13 -0600

    init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char ** argv) {
+
+    string name = "";
+
+    cout << "Hello..." << endl;
+    cout << "Tell me your name. " << endl;
+    cin >> name;
+    cout << "Hello" << name << endl << endl;
+    
+    return 0;
+}
